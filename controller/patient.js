66b00525905559ee2c55cab743da31fb538fe88c
Mausumi Patra <someepatra@gmail.com>
2019-11-06 09:50:10 -0500

    updated controllers

--- controller/patient.js
+++ controller/patient.js
@@ -2,6 +2,83 @@
 const express = require("express");
 const router = express.Router();
 //model
-const Patient = require("../models/patient");
-//seed
-const productsSeed = require("../models/seed.js");
+const Patient = require("../models/patient_schema");
+
+// router.listen(port, () => {
+//   console.log("listening on port", port);
+// });
+
+//test route
+router.get("/", (req, res) => {
+  res.send("router is running in patient");
+});
+//new
+router.get("/patients/new", (req, res) => {
+  res.render("new.ejs");
+});
+//index show all patients
+router.get("/patients", (req, res) => {
+  Patient.find({}, (error, allPatients) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.render("index.ejs", { patient: allPatients });
+    }
+  });
+});
+//create
+router.post("/patients", (req, res) => {
+  if (req.body.check_insurance === "on") {
+    req.body.check_insurance = true;
+  } else {
+    req.body.check_insurance = false;
+    req.body.insurance = "None";
+    req.body.co_pay = 100;
+  }
+  Patient.create(req.body, (error, createdPatient) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.redirect("/patients");
+    }
+  });
+});
+
+//show by patinet
+router.get("/patients/:id", (req, res) => {
+  Patient.findById(req.params.id, (error, foundPatient) => {
+    res.render("shows.ejs", { patient: foundPatient });
+  });
+});
+//delete patient data
+router.delete("/patients/:id", (req, res) => {
+  Patient.findByIdAndRemove(req.params.id, () => {
+    res.redirect("/patients");
+  });
+});
+//edit a ptient data
+router.get("/patients/:id/edit", (req, res) => {
+  Patient.findById(req.params.id, (error, foundPatient) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.render("edit.ejs", { patient: foundPatient });
+    }
+  });
+});
+//update patient
+router.put("/patients/:id", (req, res) => {
+  Patient.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedPatient) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/patients");
+      }
+    }
+  );
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = mongoose.connection;
 //port
 const port = 3000;
 //controller
-//const = require("./controllers/patient.js");
+const patient = require("./controller/patient.js");
 //config
 //const Product = require("./models/product_schema.js");
 
@@ -17,7 +17,8 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-//app.use(patientsController);
+app.use(patient);
+app.use(express.json());
 //connect mongo db
 mongoose.connect("mongodb://localhost:27017/patient_portals", {
   useNewUrlParser: true
@@ -26,82 +27,81 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
 
-app.use(express.json());
+// //test route
+// app.get("/", (req, res) => {
+//   res.send("app is running");
+// });
 //port listen
 app.listen(port, () => {
   console.log("listening on port", port);
 });
 
-//test route
-app.get("/", (req, res) => {
-  res.send("app is running");
-});
-//new
-app.get("/patients/new", (req, res) => {
-  res.render("new.ejs");
-});
-//index show all patients
-app.get("/patients", (req, res) => {
-  Patient.find({}, (error, allPatients) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.render("index.ejs", { patient: allPatients });
-    }
-  });
-});
-//create
-app.post("/patients", (req, res) => {
-  if (req.body.check_insurance === "on") {
-    req.body.check_insurance = true;
-  } else {
-    req.body.check_insurance = false;
-    req.body.insurance = "None";
-    req.body.co_pay = 100;
-  }
-  Patient.create(req.body, (error, createdPatient) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.redirect("/patients");
-    }
-  });
-});
+// //new
+// app.get("/patients/new", (req, res) => {
+//   res.render("new.ejs");
+// });
+// //index show all patients
+// app.get("/patients", (req, res) => {
+//   Patient.find({}, (error, allPatients) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.render("index.ejs", { patient: allPatients });
+//     }
+//   });
+// });
+// //create
+// app.post("/patients", (req, res) => {
+//   if (req.body.check_insurance === "on") {
+//     req.body.check_insurance = true;
+//   } else {
+//     req.body.check_insurance = false;
+//     req.body.insurance = "None";
+//     req.body.co_pay = 100;
+//   }
+//   Patient.create(req.body, (error, createdPatient) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.redirect("/patients");
+//     }
+//   });
+// });
 
-//show by patinet
-app.get("/patients/:id", (req, res) => {
-  Patient.findById(req.params.id, (error, foundPatient) => {
-    res.render("shows.ejs", { patient: foundPatient });
-  });
-});
-//delete patient data
-app.delete("/patients/:id", (req, res) => {
-  Patient.findByIdAndRemove(req.params.id, () => {
-    res.redirect("/patients");
-  });
-});
-//edit a ptient data
-app.get("/patients/:id/edit", (req, res) => {
-  Patient.findById(req.params.id, (error, foundPatient) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.render("edit.ejs", { patient: foundPatient });
-    }
-  });
-});
-//update patient
-app.put("/patients/:id", (req, res) => {
-  Patient.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, updatedPatient) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect("/patients");
-      }
-    }
-  );
-});
+// //show by patinet
+// app.get("/patients/:id", (req, res) => {
+//   Patient.findById(req.params.id, (error, foundPatient) => {
+//     res.render("shows.ejs", { patient: foundPatient });
+//   });
+// });
+// //delete patient data
+// app.delete("/patients/:id", (req, res) => {
+//   Patient.findByIdAndRemove(req.params.id, () => {
+//     res.redirect("/patients");
+//   });
+// });
+// //edit a ptient data
+// app.get("/patients/:id/edit", (req, res) => {
+//   Patient.findById(req.params.id, (error, foundPatient) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.render("edit.ejs", { patient: foundPatient });
+//     }
+//   });
+// });
+// //update patient
+// app.put("/patients/:id", (req, res) => {
+//   Patient.findByIdAndUpdate(
+//     req.params.id,
+//     req.body,
+//     { new: true },
+//     (err, updatedPatient) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.redirect("/patients");
+//       }
+//     }
+//   );
+// });
